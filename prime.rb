6cff78f0724f028e0209770f24d8455504d7af6b
Stephen Thurman <thurman024@gmail.com>
2018-01-25 16:46:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 # Add  code here!
+require 'pry'
+
 def prime?(number)
   if number <= 0
     false
   else
     list_of_num = *(1..20)
     divisor = list_of_num.detect {|x| number % x == 0}
+    binding.pry
     if divisor == number
       true
     end
